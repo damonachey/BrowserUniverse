847c946df217d4a71d4076e7ea36d8328ac166bf
Damon Achey <Damon@Achey.net>
2026-08-12 23:56:43 -0600

    Add columns for new Thing properties; move name and created into properties

--- src/Thing.js
+++ src/Thing.js
@@ -1,8 +1,7 @@
 export class Thing {
   constructor(id, name) {
     this.id = id;
-    this.name = name;
-    this.properties = {};
+    this.properties = { name, created: new Date() };
     this.relationships = [];
     this.events = [];
     this.behaviors = [];
