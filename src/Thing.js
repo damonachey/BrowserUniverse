4c1a8874a54b9d8e44f997513592a6d7cd87019c
Damon Achey <Damon@Achey.net>
2026-08-12 23:53:07 -0600

    Add properties, relationships, events, behaviors, and permissions to Thing

--- src/Thing.js
+++ src/Thing.js
@@ -2,5 +2,10 @@ export class Thing {
   constructor(id, name) {
     this.id = id;
     this.name = name;
+    this.properties = {};
+    this.relationships = [];
+    this.events = [];
+    this.behaviors = [];
+    this.permissions = {};
   }
 }
